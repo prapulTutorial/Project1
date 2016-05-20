0dc4c6937510fc6e8894ba6ac23955fe12464d69
prapulKashyap <prapul@u.northwestern.edu>
2016-05-20 10:14:15 -0700

    Day update

--- src/Prapul/dateCalculator.py
+++ src/Prapul/dateCalculator.py
@@ -1,1 +1,3 @@
-date = {0:"Sunday",1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday"}
+
+def getDay(date):
+    day = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday"}
