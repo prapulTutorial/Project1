2233fbc20a8739e4d392bd98758fa43fa65d5209
Sibison <sibiengg2014@gmail.com>
2016-05-26 07:40:43 -0700

    5/26

---
+++ src/sibison/2classsub.py
@@ -0,0 +1,14 @@
+class First(object):
+    def __init__(self):
+        print "first"
+
+class Second(object):
+    def __init__(self):
+        print "second"
+
+class subclass(First, Second):
+    def __init__(self):
+        super(subclass, self).__init__()
+
+
+s1=
